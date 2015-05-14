ad6dca8ce40c270572425385658bd1a1edb7b522
Sam Pratt <samj.pratt@gmail.com>
2015-05-14 19:09:58 +0100

    Merge merchant creation requests

--- src/Subscriber/MerchantSubscriber.php
+++ src/Subscriber/MerchantSubscriber.php
@@ -6,7 +6,7 @@ use Expressly\Event\MerchantEvent;
 use Expressly\Event\MerchantHostEvent;
 use Expressly\Event\MerchantLocationEvent;
 use Expressly\Event\MerchantNewPasswordEvent;
-use Expressly\Event\PasswordedEvent;
+use Expressly\Event\MerchantRegisterEvent;
 use Silex\Application;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
@@ -24,47 +24,25 @@ class MerchantSubscriber implements EventSubscriberInterface
     public static function getSubscribedEvents()
     {
         return array(
-            'merchant.host' => array('onHostSend', 0),
-            'merchant.password.create' => array('onPasswordCreate', 0),
+            'merchant.register' => array('onCreate', 0),
             'merchant.password.update' => array('onPasswordUpdate', 0)
         );
     }
 
-    public function onHostSend(MerchantLocationEvent $event)
+    public function onRegister(MerchantRegisterEvent $event)
     {
-        $route = $this->routeProvider->merchant_host_send;
+        $route = $this->routeProvider->merchant_register;
 
         $response = $route->process(function ($request) use ($event) {
             $merchant = $event->getMerchant();
 
-            $request->addHeader($event->getPassword());
             $request->setContent(array(
                 'referer' => array(
                     'merchant' => $merchant->getHost()
                 ),
                 'query' => array(
+                    'newPass' => $event->getPassword(),
                     'location' => $event->getLocation()
-                )
-            ));
-        });
-
-        $event->setResponse($response);
-    }
-
-    public function onPasswordCreate(PasswordedEvent $event)
-    {
-        $route = $this->routeProvider->merchant_password_update;
-
-        $response = $route->process(function ($request) use ($event) {
-            $merchant = $event->getMerchant();
-
-            $request->addHeader($event->getPassword());
-            $request->setContent(array(
-                'referer' => array(
-                    'merchant' => $merchant->getHost()
-                ),
-                'query' => array(
-                    'newPass' => $event->getPassword()
                 )
             ));
         });
