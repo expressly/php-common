3fe177c6fbc75e46396858fbb73ac3eeb2a7ea53
Sam Pratt <samj.pratt@gmail.com>
2015-05-18 16:41:29 +0100

    JS Provider

--- bootstrap/services.php
+++ bootstrap/services.php
@@ -3,6 +3,7 @@
 // Register ServiceProviders
 $app->register(new Expressly\ServiceProvider\ExternalRouteServiceProvider());
 $app->register(new Expressly\ServiceProvider\MerchantServiceProvider());
+$app->register(new Expressly\ServiceProvider\JavaScriptServiceProvider());
 
 // Register events
 $app['dispatcher']->addSubscriber(new Expressly\Subscriber\CustomerSubscriber($app));
