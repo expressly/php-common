58f86cab0e8c3502cd4f2ae2c2d9f538ae24c18c
Sam Pratt <samj.pratt@gmail.com>
2015-05-22 13:21:15 +0100

    Allow 0 values to be shown in JSON representation

--- src/Entity/ArraySerializeable.php
+++ src/Entity/ArraySerializeable.php
@@ -9,7 +9,9 @@ abstract class ArraySerializeable
     public function toArray()
     {
         $data = get_object_vars($this);
-        $data = array_filter($data);
+        $data = array_filter($data, function($item) {
+            return !is_null($item) && $item !== '';
+        });
 
         array_walk_recursive($data, function (&$element) {
             if ($element instanceof ArraySerializeable) {
