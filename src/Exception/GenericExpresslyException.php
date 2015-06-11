5fe25ebd0a098ed78011214c34d41ce7ee219ad9
Sam Pratt <samj.pratt@gmail.com>
2015-06-11 11:39:17 +0100

    Add generic expressly exception with cleaner formatting (for redis)

---
+++ src/Exception/GenericExpresslyException.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Expressly\Exception;
+
+class GenericExpresslyException extends \Exception
+{
+    public function __toString()
+    {
+        $time = new \DateTime('now', new \DateTimeZone('UTC'));
+
+        return (string)sprintf(
+            '[%s] %s (%s::%u)',
+            $time->format(\DateTime::ISO8601),
+            $this->getMessage(),
+            $this->getFile(),
+            $this->getLine()
+        );
+    }
+}
