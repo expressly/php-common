37b460442d03b521db52a9c652ad2ba6f0d0d8fe
Sam Pratt <samj.pratt@gmail.com>
2015-09-24 16:22:16 +0100

    add email validator, move email to route definition for banner request

---
+++ src/Validator/EmailValidator.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Expressly\Validator;
+
+class UuidValidator implements ValidatorInterface
+{
+    private $message;
+
+    public function __construct($message)
+    {
+        $this->message = $message;
+    }
+
+    public function validate($data)
+    {
+        if (!is_string($data)) {
+            return false;
+        }
+
+        if (filter_var($data, FILTER_VALIDATE_EMAIL)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    public function getType()
+    {
+        return 'email';
+    }
+}
