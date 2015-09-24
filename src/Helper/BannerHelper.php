5512266a7b6da5beb33c61a63d0ac4cd3494e705
Sam Pratt <samj.pratt@gmail.com>
2015-09-24 18:27:42 +0100

    add banner helper to transform event to html

---
+++ src/Helper/BannerHelper.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Expressly\Helper;
+
+use Expressly\Event\BannerEvent;
+
+class BannerHelper
+{
+    public static function toHtml(BannerEvent $event)
+    {
+        $content = $event->getContent();
+
+        if (!array_key_exists('migrationLink', $content) || !array_key_exists('bannerImageUrl', $content)) {
+            return '';
+        }
+
+        return sprintf(
+            '<div class="expressly-banner"><a href="%s"><img src="%s"/></a>',
+            $content['migrationLink'],
+            $content['bannerImageUrl']
+        );
+    }
+}
