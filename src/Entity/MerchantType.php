32ec030ac5186adc6dca4bb679633b2a627e8e70
Sam Pratt <samj.pratt@gmail.com>
2015-11-25 17:35:18 +0000

    add magento 2 merchant type

--- src/Entity/MerchantType.php
+++ src/Entity/MerchantType.php
@@ -6,6 +6,7 @@ class MerchantType
 {
     const PRESTASHOP = 'prestashop';
     const MAGENTO = 'magento';
+    const MAGENTO_2 = 'magento2';
     const OPENCART_1 = 'opencart1';
     const OPENCART_2 = 'opencart2';
     const WOOCOMMERCE = 'woocommerce';
