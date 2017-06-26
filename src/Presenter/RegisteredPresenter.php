033bf74ad31c586823354f54b0f2234889567f8b
Marc <marc.g.smith@gmail.com>
2017-06-27 00:21:41 +0100

    v2.7.1 - added platform to registered presenter

--- src/Presenter/RegisteredPresenter.php
+++ src/Presenter/RegisteredPresenter.php
@@ -4,12 +4,23 @@ namespace Expressly\Presenter;
 
 class RegisteredPresenter implements PresenterInterface
 {
+    private $platformName = null;
+    private $platformVersion = null;
+
+    public function __construct($platformName = null, $platformVersion = null)
+    {
+        $this->platformName = $platformName;
+        $this->platformVersion = $platformVersion;
+    }
+
     public function toArray()
     {
         return array(
             'registered' => true,
             'lightbox' => 'javascript',
-            'version' => 'V2'
+            'version' => 'V2',
+            'platformName' => $this->platformName,
+            'platformVersion' => $this->platformVersion
         );
     }
 }
--- tests/Presenter/RegisteredPresenterTest.php
+++ tests/Presenter/RegisteredPresenterTest.php
@@ -12,7 +12,27 @@ class RegisteredPresenterTest extends \PHPUnit_Framework_TestCase
             array(
                 'registered' => true,
                 'lightbox' => 'javascript',
-                'version' => 'V2'
+                'version' => 'V2',
+                'platformName' => null,
+                'platformVersion' => null,
+            ),
+            $presenter->toArray()
+        );
+
+        $this->assertJson(json_encode($presenter->toArray()));
+    }
+
+    public function testToArrayWithPlatform()
+    {
+        $presenter = new RegisteredPresenter('pn', 'pv');
+
+        $this->assertEquals(
+            array(
+                'registered' => true,
+                'lightbox' => 'javascript',
+                'version' => 'V2',
+                'platformName' => 'pn',
+                'platformVersion' => 'pv'
             ),
             $presenter->toArray()
         );
